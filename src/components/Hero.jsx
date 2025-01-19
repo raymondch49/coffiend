54f930eb730f79fef42ae3f2e01838f7f5351e37
Raymond <rayistheman0@gmail.com>
2025-01-19 11:05:27 -0500

    Backbone and static stuff finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,22 @@ export default function Hero() {
 
   return (
     <>
-    
+      <h1>CoffeeTracking for Coffee <abbr title="An enthusiats of devotee">Fiends</abbr>!</h1>
+      <div className="benefits-list">
+        <h3 className="fomt-bolder">Try <span 
+        className="text-gradient"> Caffiend</span> and start ...</h3>
+        <p>✅ Tracking every coffee</p>
+        <p>✅ Measuring you blood caffeine levels</p>
+        <p>✅ Costing and quantifying your addition</p>
+      </div>
+      <div className="card info-card">
+        <div>
+          <i className="fa-solid fa-circle-info"></i>
+          <h3> Did you know ...</h3>
+        </div>
+        <h5> That caffeine&apos;s half-life is about 5 hours</h5>
+        <p> This means that after 5 hours, half of the caffeine you consume is still in your system, keeping you alert longer! So if drink a cup of coffee with 200 mg of caffiene, 5 hours, later, you&apos;ll still have about 100 mg of caffeine in your system.</p> {/* &apos; is apostrophe in html symbol cause React dont like it */}
+      </div>
     </>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,35 @@
 
 export default function Layout(props) {
-  const { children } = props
+  const { children } = props   // Get everything inside Layout tag of App.jsx
 
   const header = (
-    <header></header>
+    <header>
+      <div>
+        <h1 className="text-gradient"> CAFFIEND </h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p> Sign up free </p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
   )
 
   const footer = (
-    <footer></footer>
+    <footer>
+      <p> <span className="text-gradient">Caffiend</span> made by <a>Raymond</a></p>
+    </footer>
   )
 
   return (
     <>
-    <header></header>
+    {header}
     <main>
-      {children}         {/* This helps maintain orginization  of the code with the rendering of different pages */}
+      {children}    {/* Children is special prop always avaliable passed down and represents what is nested inside Layout tag
+                      of App.jsx   
+                      This helps maintain orginization  of the code with the rendering of different pages */}
     </main>
-    <footer></footer>
+    {footer}
     </>
   )
 
