588bbcaccf99c1d14a6d3da0f1cb104d0a455c95
Raymond <rayistheman0@gmail.com>
2025-01-19 23:38:46 -0500

    Updated using global state the information of users

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ import Hero from './components/Hero'
 import CoffeeForm from './components/CoffeeForm'
 import Stats from './components/Stats'
 import History from './components/History'
+import { useAuth } from './context/AuthContent'
+import { coffeeConsumptionHistory } from './utils'
+
 
 function App() {
+  const {globalUser, globalData, isLoading} = useAuth()
   
-  const isAuthenticated = false  /* state that determines if user is authenticated */
+  const isAuthenticated = globalUser /* state that determines if user is authenticated */
+  
+  
+
+  const isData = globalData && !!Object.keys(globalData || {}).length //if 0 entries or no data(0 entries in globalData), isData is false. Double exclamation point converts any value to boolean representation
+
+
 
   const authenticatedContent = (    /* This is info conditionally renderd if the user is authenticated */
     <>
@@ -23,7 +33,8 @@ function App() {
       <Hero />     
       <CoffeeForm isAuthenticated={isAuthenticated} />        {/* Users see the website and can input their information and try to complete the form but will be     
                               prompte to sign up and be rejected*/}
-      {isAuthenticated && (authenticatedContent)}  {/* If isAuthenticated is true, authenticatedContent is rendered*/}
+      {isAuthenticated &&isLoading && (<p>Loading data...</p>)}
+      {(isAuthenticated && isData) && (authenticatedContent)}  {/* If isAuthenticated is true and we have data, authenticatedContent is rendered*/}
     </Layout>
   )
 }
